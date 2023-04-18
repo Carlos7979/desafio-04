94145c17db6238970be6cc3b554288a7039b41ba
Carlos7979 <carloscorredorwg@gmail.com>
2023-04-18 06:12:27 -0400

    update routes and project structure

---
+++ src/data/index.js
@@ -0,0 +1,3 @@
+const Product = require('./productManager');
+
+module.exports = { Product };
---
+++ src/data/productManager.js
@@ -0,0 +1,107 @@
+const fs = require('fs');
+
+class ProductManager {
+    constructor(path) {
+        this.path = path
+		this.products = []
+		this.idCounter = 0
+    }
+    async addProduct(title, description, price, thumbnail, code, stock) {
+		try {
+			if (!title || !description || !price || !thumbnail || !code || !stock)
+					return 'Todos los campos son obligatorios'
+			let products = await fs.promises.readFile(this.path, {encoding: 'utf8'})
+			products = JSON.parse(products)
+			if (products.some(e => e.code === code)) throw Error('El valor de code debe ser único')
+			products.push({
+				title,
+				description,
+				price,
+				thumbnail,
+				code,
+				stock,
+				id: (products.length + 1).toString()
+			});
+			const productsJSON = JSON.stringify(products, null, 2)
+			await fs.promises.writeFile(this.path, productsJSON, {encoding: 'utf8'})
+			return products
+		} catch (error) {
+			console.log(error)
+		}
+    }
+    async getProducts() {
+		try {
+			console.log(this.path);
+			let products = await fs.promises.readFile(this.path, {encoding: 'utf8'})
+			products = JSON.parse(products)
+			return products
+		} catch (error) {
+			console.log(error)
+		}
+    }
+    async getProductById(id) {
+		try {
+			let products = await fs.promises.readFile(this.path, {encoding: 'utf8'})
+			products = JSON.parse(products)
+			const product = products.find(e => e.id === id)
+			if (product) return product
+			// else return `Error, el producto con id ${id} no se ha encontrado`
+			else return { error: `El producto con el id ${id} no existe` }
+		} catch (error) {
+			console.log(error)
+		}
+    }
+	async updateProduct(id, object) {
+        try {
+            let products = await fs.promises.readFile(this.path, { encoding: 'utf8' });
+            products = JSON.parse(products);
+            let index;
+            const product = products.find((e, i) => {
+                if (e.id === id) {
+                    index = i;
+                    return true;
+                }
+            });
+            if (!product) return 'Not found';
+            for (const key in object) {
+                if (Object.hasOwnProperty.call(product, key) && key !== 'id') {
+                    if (key !== 'code') product[key] = object[key];
+                    else {
+						if (products.some((e, i) => e.code === object[key] && i !== index))
+                            return 'El valor de code debe ser único';
+                    }
+                }
+            }
+            products[index] = product;
+            const productsJSON = JSON.stringify(products, null, 2);
+            await fs.promises.writeFile(this.path, productsJSON, { encoding: 'utf8' });
+            return product;
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async deleteProduct(id) {
+        try {
+            let products = await fs.promises.readFile(this.path, { encoding: 'utf8' });
+            products = JSON.parse(products);
+            let index;
+            const product = products.find((e, i) => {
+                if (e.id === id) {
+                    index = i;
+                    return true;
+                }
+            });
+            if (!product) return 'Not found';
+            const deleted = products.splice(index, 1)[0];
+            const productsJSON = JSON.stringify(products, null, 2);
+            await fs.promises.writeFile(this.path, productsJSON, { encoding: 'utf8' });
+            return deleted;
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+const productManager = new ProductManager('db/ProductManager.json')
+
+module.exports = productManager
